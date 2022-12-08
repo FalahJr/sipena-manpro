ac90b4a037d536f027aa4ee60015ca53b8b67569
Muhammad Ferdy Cahya Pratama <ferdyp73@gmail.com>
2022-12-08 11:26:53 +0700

    data login

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -23,7 +23,7 @@ class loginController extends Controller
     public function loginApi(Request $req) {
         $username = $req->username;
         $password = $req->password;
-        $user = DB::table("user")->select("user.*", "role.*", "user.id as id", "role.id as roleid" "role.nama as rolenama", "user.created_at as data", "user.created_at as role")->where("username", $username)->join("role", "role.id", '=', "user.role_id")->first();
+        $user = DB::table("user")->select("user.*", "role.*", "user.id as id", "role.id as roleid", "role.nama as rolenama", "user.created_at as data", "user.created_at as role")->where("username", $username)->join("role", "role.id", '=', "user.role_id")->first();
 
         if ($user && $user->password == $password) {
 
