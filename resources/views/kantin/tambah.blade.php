478e76e946f5e0c89745b6aa1152f94293dafa14
gitapin82y <gitapin82y@gmail.com>
2022-12-15 12:19:51 +0700

    repair kantin koperasi qrcode and api kartu digital

--- resources/views/kantin/tambah.blade.php
+++ resources/views/kantin/tambah.blade.php
@@ -12,7 +12,8 @@
         <div class="row">
           <table class="table table_modal">
             <div class="alert alert-warning" role="alert">
-              Silahkan isi semua data yang bertanda<span style="color:red;">*</span>
+              Silahkan isi semua data yang bertanda<span style="color:red;">*</span><br>
+              Pastikan saldo anda cukup<span style="color:red;">*</span>
               <br>QRCode akan dibuat setelah menambahkan data kantin
             </div>
           <tr>
