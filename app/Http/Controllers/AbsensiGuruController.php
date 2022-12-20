6b9209a8e73a5401620f71ca7c75090d13e3fca8
Muhammad Ferdy Cahya Pratama <ferdyp73@gmail.com>
2022-12-20 10:55:55 +0700

    Fix

--- app/Http/Controllers/AbsensiGuruController.php
+++ app/Http/Controllers/AbsensiGuruController.php
@@ -29,6 +29,8 @@ class AbsensiGuruController extends Controller
             ->select("guru.*", "guru_absensi.*", "guru_absensi.id as terlambat")
             ->get()->toArray();
 
+            dd($data);
+
             foreach ($data as $key => $value) {
               $waktu = Carbon::parse($value->waktu)->format('H:i:s');
               $batas = "06:00:00";
