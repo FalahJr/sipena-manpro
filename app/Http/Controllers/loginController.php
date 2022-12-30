a0221820b0420ef7c195dbd79d48394ea48d1fce
Muhammad Ferdy Cahya Pratama <ferdyp73@gmail.com>
2022-12-30 13:28:11 +0700

    Get siswa

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -130,21 +130,18 @@ class loginController extends Controller
                 $ceksiswa = DB::table("siswa")->select("siswa.*", "siswa.id as kelas")->where("wali_murid_id", $cekdata->id)->get();
 
                 if($ceksiswa != null) {
-                  $inIDSiswa = [];
-
-                  foreach ($ceksiswa as $key => $value) {
-                      $inIDSiswa[$key] = $value->kelas_id;
-                  }
-
-                  $cekkelas = DB::table("kelas")->whereIn("id", $inIDSiswa)->get();
-
                   $user->siswa = $ceksiswa;
 
-                  if($cekkelas != null) {
-                    $user->siswa->kelas = $cekkelas;
-                  } else {
-                    $user->siswa->kelas = $oVal;
+                  foreach ($ceksiswa as $key => $value) {
+                      $cekkelas = DB::table("kelas")->whereIn("id", $value->kelas_id)->get();
+
+                      if($cekkelas != null) {
+                        $user->siswa->kelas = $cekkelas;
+                      } else {
+                        $user->siswa->kelas = $oVal;
+                      }
                   }
+
                 } else {
                   $user->siswa = $oVal;
                 }
